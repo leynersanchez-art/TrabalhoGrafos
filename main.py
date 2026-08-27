494f7bd8016c8b458a8a334e7d4e4560d78d9698
Leyner <leyner.sanchez@aluno.ufca.edu.br>
2026-08-27 16:57:16 -0300

    Refactor main to include region graph details
    Updated main function to use GrafoRegiao and display region data.

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from entidades import Pokemon, Treinador
-from grafo import Grafo
+from grafo import GrafoRegiao
 
 def main():
-    mapa = Grafo()
-    mapa.carregar_mapa_arquivo("Aqui o caminho do seu arquivo")
-
-    treinador = Treinador("Ash")
-    print("Iniciando a Jornada pokemon: ")
+    mapa = GrafoRegiao()
+    
+    print("--- DADOS DA REGIÃO ---")
+    print(f"Pokémons espalhados: {mapa.num_pokemons}")
+    print(f"Treinadores espalhados: {mapa.num_treinadores}")
+    print(f"Itens extras: {mapa.num_itens}")
+    
+    prazo_min = 10 * mapa.soma_pesos
+    prazo_max = 15 * mapa.soma_pesos
+    print(f"Prazo para a Liga Pokémon: entre {prazo_min} e {prazo_max} unidades de tempo")
+    
+    print("\n--- ROTAS DO MAPA (LISTA DE ADJACÊNCIA) ---")
+    for local, rotas in mapa.adjacencias.items():
+        print(f"{local} se conecta com: {rotas}")
 
 if __name__ == "__main__":
     main()
